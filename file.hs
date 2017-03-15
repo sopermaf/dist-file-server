ca0def738f1232dc4eb3edbb245af0dd13465f7d
sopermaf <sopermaf@tcd.ie>
2017-03-15 19:07:42 +0000

    fileServer list directory files

--- file.hs
+++
@@ -1,7 +0,0 @@
-import System.Environment
-
-   main = do
-       [f,g] <- getArgs
-       s     <- readFile f
-       writeFile g s
-
---
+++ fileServer.hs
@@ -0,0 +1,11 @@
+module FileServer where
+
+import System.Directory
+
+--upload
+--download
+--delete
+--listFiles
+
+listFiles = getDirectoryContents "." --not pretty but works
+
