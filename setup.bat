1c3cecc0b76f4ad7153c7544a8a6e68b486627ac
sopermaf <sopermaf@tcd.ie>
2017-03-26 18:04:29 +0100

    added bash script to simplfy setup

---
+++ setup.bat
@@ -0,0 +1,16 @@
+set message=C:\Users\Ferdia\Desktop\DISTRIBUTTED\fileServer\
+echo %message%
+
+
+start python %message%fileServer.py
+start python %message%fileServer.py
+start python %message%fileServer.py
+
+pause
+
+start python %message%authenticationServ.py
+start python %message%directoryServ.py
+
+start python %message%client.py
+
+pause
